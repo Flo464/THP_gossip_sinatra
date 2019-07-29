210853d1969a5c915ccbe308edd72b624db72a63
Flora <caponi.flora@gmail.com>
2019-07-29 16:58:25 +0200

    update

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,31 +1,5 @@
 
 require_relative 'gossip'
-require_relative 'view'
-
-class Controller
-
-	def initialize
-		@view = View.new
-	end 
-
-	def create_gossip
-		params = @view.create_gossip
-		gossip = Gossip.new(params[:author],params[:content])
-		gossip.save
-	end 
-
-	def index_gossips
-		gossip_array = Gossip.all
-		@view.index_gossips(gossip_array)
-	end 
-
-	def destroy_gossip
-		params = @view.destroy_gossip
-		Gossip.destroy (number)
-	
-	end 
-
-end 
 
 class ApplicationController < Sinatra::Base
 
@@ -46,12 +20,9 @@ class ApplicationController < Sinatra::Base
   	erb :gossips, locals: {gossips: Gossip.all}
 	end
 
-	get '/gossips/:id' do 
-		erb :show_gossips, locals: {id: params[id], found: Gossip.find(id)}
-	end 
-
-	post '/gossip/:id/' do 
-		Gossip.find(:id)
+	get '/:id' do 
+		Gossip.find(['id'])
+		erb :show_gossip, locals: {id: params[id], found: Gossip.find(id)}
 	end 
 
 end
